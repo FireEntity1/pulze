0fd247ce8b3e6c7dcd1cf2449b837b446edcdabc
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-06-03 17:37:01 -0600

    feat: loading playlists

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from "svelte";
+	// import { splitVendorChunk } from "vite";
 
 	let player;
 
@@ -10,7 +11,7 @@
 				width: '640',
 				playerVars: {
 					listType: 'playlist',
-					list: 'RDsF80I-TQiW0',
+					list: '',
 					autoplay: 1,
 					controls: 0,
                     rel: 0,
@@ -43,7 +44,39 @@
 			console.error("player brokey");
 		}
 	}
-</script>
+
+    function play() {
+		if (player && player.nextVideo) {
+            if (player.getPlayerState() == 1) {
+                player.pauseVideo();
+            } else if (player.getPlayerState() == 2 || player.getPlayerState() == -1) {
+                player.playVideo();
+            }
+		} else {
+			console.error("player brokey");
+		}
+	}
+
+	function loadPlaylist() {
+		event.preventDefault();
+		let loaded = false;
+		let url = document.getElementById("link").value;
+		let splitUrl = url.split("&");
+		for (let i = 0; i < splitUrl.length; i++) {
+			if (splitUrl[i].includes("list=")) {
+				let listId = splitUrl[i].replace("list=", "");
+				player.loadPlaylist({list: listId});
+				player.playVideo();
+				loaded = true;
+			}
+		}
+		if (!loaded) {
+			player.loadPlaylist({list: url})
+			player.playVideo();
+		}
+	}
+// https://www.youtube.com/watch?v=lc4nTM6M9KY&list=PLoogdHvYrJhlwTqkvGzds6BpT65nCBD28&index=1
+</script> 
 
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -53,14 +86,17 @@
 />
 
 <div class="fixed inset-0 w-screen h-screen z-[-1] bg text-center items-center flex flex-col">
-
     <div class="m-2">
-        <h1 class="font text-6xl justify-center">pulze</h1>
+        <h1 class="font text-6xl justify-center" id="title">pulze</h1>
     </div>
+	<form on:submit={loadPlaylist}>
+		<input id="link">
+	</form>
     <div id="player" class="justify-center flex"></div>
     <div class="m-4">
         <button on:click={previous} class="font text-2xl bg-slate-900 p-7 rounded-3xl">Previous</button>
         <button on:click={next} class="font text-2xl bg-slate-900 p-7 rounded-3xl">Next</button>
+        <button on:click={play} class="font text-2xl bg-slate-900 p-7 rounded-3xl">||</button>
     </div>
 
 </div>
